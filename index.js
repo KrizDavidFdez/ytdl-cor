1a5d411d7b3fb0b4c757870f6f6c94bf6e65c5ce
Samuel_4/7 <147653194+KrizDavidFdez@users.noreply.github.com>
2024-07-28 22:15:10 -0500

    index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.xml'));
 });
 
 app.post('/download', async (req, res) => {
